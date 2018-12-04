5dcd6fc6fadc1b10243b38574d3531c61d3ca1fa
bembolat <barkadwood@gmail.com>
2018-12-04 12:55:11 +0300

    Начало верстки

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var imagemin = require("gulp-imagemin");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var webp = require("gulp-webp");
+var del = require("del");
+var minify = require("gulp-csso");
+
+gulp.task("style", function() {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", ["style"],function() {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{sass,scss}", gulp.series("style"));
+  gulp.watch("source/*.html").on("change", server.reload);
+})
+
+  gulp.task("start", gulp.series("style", "serve"));
+
+
