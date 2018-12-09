75c6bec60c213dfae73333a7c2829020f857a44d
bembolat <barkadwood@gmail.com>
2018-12-10 02:18:07 +0300

    завершил верстку проекта

--- source/js/main.js
+++ source/js/main.js
@@ -1,18 +1,56 @@
-var nav = document.querySelector(".menu__list");
 var navBtn = document.querySelector(".menu__button");
+var menu = document.querySelector(".menu");
+var nav = document.querySelector(".menu__list");
+var link = document.querySelectorAll(".menu__link");
+var serviceLink = document.querySelectorAll(".service__link");
+var serviceBlock = document.querySelectorAll(".service__block");
 
-
-
+//удаляет класс автоматического открытия меню, если js на девайсе не работает по какой то причине
+menu.classList.remove("menu--no-js");
+//открывает или закрывает меню по нажатию на кнопку
 navBtn.addEventListener("click", function () {
-  if (nav.classList.contains("menu__list--closed")) {
-    nav.classList.remove("menu__list--closed");
-    nav.classList.add("menu__list--opened");
+    menu.classList.toggle("menu--opened");
+});
+//закрывает меню при нажатии кнопки Esc
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (menu.classList.contains("menu--opened")) {
+      menu.classList.remove("menu--opened");
+ 
+    } 
   }
-  else {
-    nav.classList.remove("menu__list--opened");
-    nav.classList.add("menu__list--closed");
-  }
+});
+//закрывает меню по клику на ссылку
+for (var x = 0; x < link.length; x++) {
+            link[x].addEventListener("click", function(evt) {
+                menu.classList.remove("menu--opened");
+            });
+        }
+//Слайдер в header
+var slides = document.querySelectorAll(".slider__item");
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide() {
+    slides[currentSlide].className = "slider__item";
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = "slider__item slider__item--active";
+}
+//Слайдер для раздела "Наши услуги"
+  for (var i = 0; i < serviceLink.length; i++)  {
+  serviceLink[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    document.querySelector(".service__item--active").classList.remove("service__item--active");
+    this.parentNode.classList.add("service__item--active");
+    for (var y = 0; y < serviceLink.length; y++) {
+      if (serviceLink[y] === this) {
+        document.querySelector(".show").classList.remove("show");
+        serviceBlock[y].classList.add("show");
+      }
+    }
 })
+}
 
 
 
