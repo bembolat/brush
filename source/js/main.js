e647feff9ba006a963fd84ef2a88b91cfea4dc85
bembolat <barkadwood@gmail.com>
2018-12-10 17:53:59 +0300

    написал скрипт для кнопки наврех

--- source/js/main.js
+++ source/js/main.js
@@ -27,7 +27,7 @@ for (var x = 0; x < link.length; x++) {
                 menu.classList.remove("menu--opened");
             });
         }
-//Слайдер в header
+/*//Слайдер в header
 var slides = document.querySelectorAll(".slider__item");
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,2000);
@@ -36,7 +36,7 @@ function nextSlide() {
     slides[currentSlide].className = "slider__item";
     currentSlide = (currentSlide+1)%slides.length;
     slides[currentSlide].className = "slider__item slider__item--active";
-}
+}*/
 //Слайдер для раздела "Наши услуги"
   for (var i = 0; i < serviceLink.length; i++)  {
   serviceLink[i].addEventListener("click", function (evt) {
@@ -52,5 +52,88 @@ function nextSlide() {
 })
 }
 
+//jQuery
 
+  $(document).ready(function () {
+
+  var change_img_time = 3000,
+      transition_speed = 400;
+
+  var listItems = $(".slider__list").children('li'),
+      dotItems = $('.controls').children('li'),
+      listLen = listItems.length,
+      current,
+      changeTimeout;
+
+  function moveTo(newIndex) {
+
+    var i = newIndex;
+
+    if (newIndex == 'prev') {
+        i = (current > 0) ? (current - 1) : (listLen - 1);
+    }
+
+    if (newIndex == 'next') {
+        i = (current < listLen - 1) ? (current + 1) : 0;
+    }
+
+    dotItems.removeClass('active')
+            .eq(i).addClass('active');
+
+    listItems.fadeOut(transition_speed)
+             .eq(i).fadeIn(transition_speed);
+
+    current = i;
+
+    //resets time interval if user clicks on slider dot; then begin automated slider
+    clearTimeout(changeTimeout);
+    changeTimeout = setTimeout(function() { moveTo('next'); }, change_img_time);
+  };
+
+  // Event handlers
+  $(".controls li").click(function () {
+    var i = $('.controls li').index(this);
+    moveTo(i);
+  });
+
+  $("#prev").click(function () {
+    moveTo('prev');
+  });
+
+  $("#next").click(function () {
+    moveTo('next');
+  });
+  
+  //initialize slider on load
+  moveTo('next');
+});
+
+//Кнопка вверх
+(function() {
+  'use strict';
+
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      goTopBtn.classList.add('button-top--show');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('button-top--show');
+    }
+  }
+
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(backToTop, 0);
+    }
+  }
+
+  var goTopBtn = document.querySelector('.button-top');
+
+  window.addEventListener('scroll', trackScroll);
+  goTopBtn.addEventListener('click', backToTop);
+})();
 
